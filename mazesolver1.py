4a143ac8370340cc4c41f341d31903ce80510b9c
Gabor Szita <gaborszita88@gmail.com>
2021-08-10 11:04:37 -0700

    Add GNU GPLv3 license.

--- mazesolver1.py
+++ mazesolver1.py
@@ -1,3 +1,21 @@
+# Copyright 2018, 2021 Gabor Szita
+# 
+# This file is part of Raspberry Pi maze solver.
+# 
+# Raspberry Pi maze solver is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Raspberry Pi maze solver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Raspberry Pi maze solver.  If not, see 
+# <https://www.gnu.org/licenses/>.
+
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 from mazesolverADCpi import *
